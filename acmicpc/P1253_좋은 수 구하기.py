1977e82221c6f4e4cffa21a26c3e2ae712d3ae01
jaeinkim <top100itw@nate.com>
2022-11-06 11:05:22 +0900

    test

--- acmicpc/P1253_좋은 수 구하기.py
+++ acmicpc/P1253_좋은 수 구하기.py
@@ -1,44 +1,81 @@
 #책의 코드
-n = int(input())
-count = 1
-start_index = 1
-end_index = 1
-sum = 1
+import sys
+input = sys.stdin.readline
+N = int(input())
+Result = 0
+A = list(map(int, input().split()))
+A.sort()
 
-while end_index != n:
-    if sum == n:  # 정답 케이스
-        count += 1
-        end_index += 1
-        sum += end_index
-    elif sum > n:
-        sum -= start_index
-        start_index += 1
-    else:
-        end_index += 1
-        sum += end_index
-
-print(count)
-
-
-
-
+for k in range(N):
+    find = A[k]
+    i = 0
+    j = N-1
+    while i < j: # 투 포인터 알고리즘
+        if A[i] + A[j] == find:
+            if i != k and j != k:
+                Result +=1
+                break
+            elif i == k:
+                i += 1
+            elif j == k:
+                j -= 1
+        elif A[i] + A[j] < find:
+            i += 1
+        else:
+            j -= 1
+print(Result)
 
 # 나의 코드
-n  = int(input())
-start_index = 1
-end_index = 1
-sum = 1
-count = 1
+n = int(input())
+A = list(map(int, input().split()))
+A.sort()
+count = 0
 
-while end_index != n:
-    if n == sum:
-        end_index += 1
-        sum += end_index
-        count += 1
-    elif n > sum:
-        end_index+=1
-        sum += end_index
-    else:
-        sum -= start_index
-        start_index += 1
-print(count)
+if n <3:
+    print(0)
+
+else:
+    for k in range(n-1, 1, -1):
+        m = A[k]
+        i = 0
+        j = k-1
+        while i !=j:
+            if A[i] + A[j] > m:
+                j-=1
+            elif A[i] + A[j] < m:
+                i+=1
+            else:
+                count+=1
+                # print(f'i, j = {i}, {j}')
+                # print(f'm = {m}')
+                # print(f'm=')
+                break
+
+    print(count)
+
+# 나의 코드 #2: 정답 보고
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+A.sort()
+result = 0
+for k in range(N):
+    find = A[k]
+    i = 0
+    j = N-1
+    while i < j:
+        if A[i] + A[j] == find:
+            if i == k:
+                i+=1
+            elif j == k:
+                j-=1
+            else:
+                result +=1
+                break
+        elif A[i] + A[j] > find:
+            j-=1
+        else:
+            i += 1
+print(result)
