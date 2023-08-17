c1bed0a41591720f3f05810defa616a798e657dc
jaeinkim <top100itw@nate.com>
2023-08-17 11:14:23 +0900

    추가

--- leetcode/20. Valid Parentheses.py
+++ leetcode/20. Valid Parentheses.py
@@ -1,15 +1,13 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        hashmap = { 'M': 1000, 'CM':900,  'D':500,'CD': 400,    'C': 100, 'XC':90, 'L': 50, 'XL':40, 'X': 10, 'IX': 9,   'IV': 4, 'V': 5,  'III': 3,  'I': 1}
-        answer = 0
-        for k, v in hashmap.items():
-            while True:
-                if k == s[:len(k)]:
-                    print(f'k= {k}')
-                    print(f'v = {v}')
-                    answer += v
-                    s = s[len(k):]
-                    print(f's={s}')
+    def isValid(self, s: str) -> bool:
+        map = {')': '(', '}': '{', ']':'['}
+        stack = []
+        for c in s:
+            if c in map:
+                if stack and stack[-1] == map[c]:
+                    stack.pop()
                 else:
-                    break
-        return answer
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
