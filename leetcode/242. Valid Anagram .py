3f53b45c5b047d7b38d3a68d0eb5f80c88ba56f9
jaeinkim <top100itw@nate.com>
2023-08-29 08:04:41 +0900

    중간본 추가

--- leetcode/242. Valid Anagram .py
+++ leetcode/242. Valid Anagram .py
@@ -1,6 +1,17 @@
-import collections
+# 1. collections 시도
+# import collections
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         v_s = collections.Counter(s)
+#         v_t = collections.Counter(t)
+#         return v_s == v_t
+
+
+# 2. hm 이용
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        v_s = collections.Counter(s)
-        v_t = collections.Counter(t)
+        for c in s:
+
+        hm_s = collections.Counter(s)
+        hm_t = collections.Counter(t)
         return v_s == v_t
