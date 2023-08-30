d1a4316e1e1f24cf4d7cc37833aeecc19b39d513
jaeinkim <top100itw@nate.com>
2023-08-31 07:47:40 +0900

    중간본 추가

--- leetcode/242. Valid Anagram .py
+++ leetcode/242. Valid Anagram .py
@@ -10,8 +10,13 @@
 # 2. hm 이용
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        hm_s = {}
+        hm_t = {}
         for c in s:
+            hm_s[c] = hm_s.get(c, 0) + 1
+        for c in t:
+            hm_t[c] = hm_t.get(c, 0) + 1
+        print(hm_s, hm_t)
+        return hm_s == hm_t
 
-        hm_s = collections.Counter(s)
-        hm_t = collections.Counter(t)
-        return v_s == v_t
+
