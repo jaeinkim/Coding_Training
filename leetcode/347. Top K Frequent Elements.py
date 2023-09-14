f434f2bcb3834519a47f1d055e6974d57d25c566
jaeinkim <top100itw@nate.com>
2023-09-15 06:23:29 +0900

    347. Top K Frequent Elements 수정

--- leetcode/347. Top K Frequent Elements.py
+++ leetcode/347. Top K Frequent Elements.py
@@ -16,3 +16,29 @@ class Solution:
             answer.append(hm2[arr[i]])
         return answer
 
+
+# 정답보고 해서 성공
+import collections
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 이중 어레이를 써보자.  어레이의 인덱스를 카운트로 하자
+        lst = [[] for i in range(len(nums)+1)]
+        hm = {}
+        for n in nums:
+            hm[n] = hm.get(n, 0) + 1
+
+        for K, V in hm.items():
+            lst[V].append(K)
+
+        answer = []
+
+        for i in range(len(lst)-1,-1,-1):
+            k = k - len(lst[i])
+            answer = answer + lst[i]
+            if  k == 0:
+                return answer
+
+
+
+
+
