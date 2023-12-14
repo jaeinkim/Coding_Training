e2a5cc828010bf3899213bbc4f22d05d72b5939f
jaeinkim <top100itw@nate.com>
2023-12-15 08:03:20 +0900

    128. Longest Consecutive Squence 오답 복기 추가

--- leetcode/128. Longest Consecutive Sequence.py
+++ leetcode/128. Longest Consecutive Sequence.py
@@ -32,3 +32,18 @@ class Solution:
         return longest
 
 
+# 23/12/15 금. 복기 시도 결과 Time Limit Exceeded. 처음시작이지 않은 것들을 순회하는 것들이니 거의 무조건 이중으로 돈다고 생각하는 잘못된 로직이네....
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+        length = 0
+
+        for n in nums:
+            length = 1
+            if n-1 not in numSet:
+                continue
+            while n + length in numSet:
+                length +=1
+            longest = max(length+1, longest)
+        return length if longest == 0 else longest # 아예 0건인 경우를 위한 로직
