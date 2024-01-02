1b867f41a41a3bced9fd5ffbc7b4ee2e1db46b1a
jaeinkim <top100itw@nate.com>
2024-01-03 06:24:02 +0900

    20. Valid Parentheses 복기함

--- leetcode/20. Valid Parentheses.py
+++ leetcode/20. Valid Parentheses.py
@@ -11,3 +11,21 @@ class Solution:
             else:
                 stack.append(c)
         return True if not stack else False
+
+# 24.1.3 수. 복기로 품
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = list()
+        hm = {'(': ')', '{': '}', '[': ']'}
+        for c in s:
+            if c in hm.keys():
+                stack.append(hm[c])
+                # print(stack)
+            else:
+                if len(stack) !=0 and c == stack[-1] :
+                    stack = stack[0:-1]
+                    # print(f'stack ={stack}')
+                else:
+                    return False
+        return True if len(stack) == 0 else False
+
